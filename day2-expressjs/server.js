c59b37f730072f0f0f851625ecd55cbbb95190b3
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-08-18 15:39:11 +0530

    installing express

--- day2-expressjs/server.js
+++ day2-expressjs/server.js
@@ -1,17 +1,17 @@
-let http = require("http");
+// let http = require("http");
 
-let server = http.createServer((req, res) => {
-  if (req.url === "/users") {
-    res.end("me users mai hu");
-  }
-  if (req.url === "/home") {
-    res.end("me hone mai hu");
-  }
-  if (req.url === "/carts") {
-    res.end("me carts mai hu");
-  }
-});
+// let server = http.createServer((req, res) => {
+//   if (req.url === "/users") {
+//     res.end("me users mai hu");
+//   }
+//   if (req.url === "/home") {
+//     res.end("me hone mai hu");
+//   }
+//   if (req.url === "/carts") {
+//     res.end("me carts mai hu");
+//   }
+// });
 
-server.listen(3000, () => {
-  console.log("server chalu hai 3000 se");
-});
+// server.listen(3000, () => {
+//   console.log("server chalu hai 3000 se");
+// });
