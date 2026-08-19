314dd3fff3956121ddeec78ca597bae156cac8fd
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-08-19 16:30:22 +0530

    create

--- day3-CRUD/server.js
+++ day3-CRUD/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-let users = [{
-    name:"amit",
-    age:26
-}]
-// read
+// middleware
+app.use(express.json())
+let users = []
+
+// create
+app.post("/create",(req,res)=>{
+    let body = req.body;
+    users.push(body)
+    res.send(users);
+})
+
+// get - Read
 app.get("/",(req,res)=>{
     res.send(users)
     res.send("OK get it");
