2824ef291b536341fa2fd6ad2b6c478774208997
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-08-19 17:21:51 +0530

    day2 processing

--- day3-CRUD/server.js
+++ day3-CRUD/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-// middleware
+// middleware for accepting json data 
 app.use(express.json())
 let users = []
 
@@ -9,13 +9,30 @@ let users = []
 app.post("/create",(req,res)=>{
     let body = req.body;
     users.push(body)
-    res.send(users);
+    res.send("users saved succesfully");
 })
 
 // get - Read
 app.get("/",(req,res)=>{
     res.send(users)
     res.send("OK get it");
+})
+
+// update
+app.put("/update/:id",(req,res)=>{
+    let {id}=req.params;
+    let updatedUser = users.map((val)=>{
+        val
+    })
+    res.send("update hone wala hai")
+})
+// delete
+app.delete("/delete/:id",(req,res)=>{
+let {id} = req.params;
+let userData = users.filter((val)=>val.id !== id)
+users = userData;
+res.send(users)
+
 })
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
