4c3f59e49fba1a16b282773687366de7d9a2c5e6
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-08-29 12:29:24 +0530

    mongodb coneected done

--- day4-dbconnection/server.js
+++ day4-dbconnection/server.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 const port = 3000;
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("mongodb connected");
+  } catch (error) {
+    console.log("error while connecting db", error);
+  }
+};
+
+connectDb();
+
 app.get("/", (req, res) => {
   res.send("Ok get it");
 });
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
