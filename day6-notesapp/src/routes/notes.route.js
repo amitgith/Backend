eb5a5d5f5e8c5fef7b06b24341359800fe7d0845
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-08-31 09:49:28 +0530

    day6-adding comments

--- day6-notesapp/src/routes/notes.route.js
+++ day6-notesapp/src/routes/notes.route.js
@@ -7,9 +7,16 @@ const {
   deleteNotesController,
 } = require("../controllers/notes.controller");
 const router = express.Router();
+//create notes
 router.post("/create", createNotesController);
+// read all
 router.get("/allNotes", getAllnotesController);
+//read one
 router.get("/:id", getSingleNoteController);
+//update via patch
 router.put("/:id", updatedNotesController);
+//update via patch
+//soon
+//delete
 router.delete("/:id", deleteNotesController);
 module.exports = router;
