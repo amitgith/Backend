3cfb9b6159fdbf30a5a79d3135fc9d79b0d4f2e5
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-09-06 21:34:56 +0530

    processing

--- day7-revision/backend/src/controllers/note.controller.js
+++ day7-revision/backend/src/controllers/note.controller.js
@@ -12,9 +12,45 @@ export const createNoteController = async (req, res) => {
       data: newNote,
     });
   } catch (error) {
+    console.log(error.message);
     return res.status(500).json({
       success: false,
       message: "Internal server error",
+    });
+  }
+};
+
+export const getAllNoteContoller = async (req, res) => {
+  try {
+    const allnotes = await noteModel.find();
+    res.status(200).json({
+      message: "All notes fetched",
+      data: allnotes,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "Internal Server error",
+    });
+  }
+};
+export const getSingleNoteController = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const note = await noteModel.findById(noteId);
+    if (!note) {
+      return res.status(404).json({
+        message: "Note nahi mila",
+      });
+    }
+    res.status(200).json({
+      message: "Note fetched successfully",
+      data: note,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "Internal Server error",
     });
   }
 };
--- day7-revision/backend/src/routes/notes.route.js
+++ day7-revision/backend/src/routes/notes.route.js
@@ -1,5 +1,14 @@
 import express from "express";
-import { createNoteController } from "../controllers/note.controller.js";
+import {
+  createNoteController,
+  getAllNoteContoller,
+  getSingleNoteController,
+} from "../controllers/note.controller.js";
 const router = express.Router();
+//create
 router.post("/create", createNoteController);
+// Read
+router.get("/allNotes", getAllNoteContoller);
+// read one
+router.get("/:id", getSingleNoteController);
 export default router;
