b90d0005c5b04bbf911e97fa2bed9581af00effb
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-09-07 09:51:45 +0530

    singleupdate successfully

--- day7-revision/backend/src/controllers/note.controller.js
+++ day7-revision/backend/src/controllers/note.controller.js
@@ -20,7 +20,7 @@ export const createNoteController = async (req, res) => {
   }
 };
 
-export const getAllNoteContoller = async (req, res) => {
+export const getAllNoteController = async (req, res) => {
   try {
     const allnotes = await noteModel.find();
     res.status(200).json({
@@ -40,7 +40,7 @@ export const getSingleNoteController = async (req, res) => {
     const note = await noteModel.findById(noteId);
     if (!note) {
       return res.status(404).json({
-        message: "Note nahi mila",
+        message: "Note not found",
       });
     }
     res.status(200).json({
@@ -54,3 +54,51 @@ export const getSingleNoteController = async (req, res) => {
     });
   }
 };
+export const updateNotesController = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const body = req.body;
+    const updatedNote = await noteModel.findByIdAndUpdate(noteId, body);
+    if (!updatedNote) {
+      return res.status(403).json({
+        success: false,
+        message: "Updated note not found",
+      });
+    }
+    res.status(200).json({
+      message: "Note Updated Successfully",
+      data: updatedNote,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server error",
+    });
+  }
+};
+export const singleEntityUpdateController = async (req, res) => {
+  try {
+    const noteId = req.params.id;
+    const body = req.body;
+    const updatedId = await noteModel.findByIdAndUpdate(noteId, body);
+    if (!updatedId) {
+      return res.status(403).json({
+        success: false,
+        message: "single note not found",
+      });
+    }
+    res.status(200).json({
+      message: "Note updated Successfully",
+      data: updatedId,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+
--- day7-revision/backend/src/routes/notes.route.js
+++ day7-revision/backend/src/routes/notes.route.js
@@ -1,14 +1,23 @@
 import express from "express";
 import {
   createNoteController,
-  getAllNoteContoller,
+  deleteNoteController,
+  getAllNoteController,
   getSingleNoteController,
+  singleEntityUpdateController,
+  updateNotesController,
 } from "../controllers/note.controller.js";
 const router = express.Router();
 //create
 router.post("/create", createNoteController);
 // Read
-router.get("/allNotes", getAllNoteContoller);
+router.get("/allNotes", getAllNoteController);
 // read one
 router.get("/:id", getSingleNoteController);
+// update via put
+router.put("/:id", updateNotesController);
+// update via patch
+router.patch("/:id/single", singleEntityUpdateController);
+// delete
+router.delete("/:id", deleteNoteController);
 export default router;
